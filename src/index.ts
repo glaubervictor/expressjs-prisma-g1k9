edaa84496efcb37589cf8d32ba99fe6ca29b280c
Glauber Carvalho <glaubervictor@gmail.com>
2022-09-02 17:07:25 -0300

    feat: added cache

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import express from "express";
+import redis from "./lib/cache";
 
 const prisma = new PrismaClient();
 
@@ -10,20 +11,41 @@ app.use(express.json());
 app.use(express.raw({ type: "application/vnd.custom-type" }));
 app.use(express.text({ type: "text/html" }));
 
+const cacheKey = "artists:all";
+
 app.get("/artists", async (req, res) => {
-  const artists = await prisma.artist.findMany();
-  res.json(artists);
+  try {
+    const cachedArtists = await redis.get(cacheKey);
+
+    if (cachedArtists) {
+      return res.json(JSON.parse(cachedArtists));
+    }
+
+    const artists = await prisma.artist.findMany();
+    await redis.set(cacheKey, JSON.stringify(artists));
+
+    return res.json(artists);
+  } catch (error) {
+    return res.json({ error });
+  }
 });
 
 app.post("/artist", async (req, res) => {
   const { name } = req.body;
-  const artist = await prisma.artist.create({
-    data: {
-      name
-    },
-  });
 
-  return res.json(artist);
+  try {
+    const artist = await prisma.artist.create({
+      data: {
+        name,
+      },
+    });
+
+    redis.del(cacheKey);
+
+    return res.json(artist);
+  } catch (error) {
+    return res.json({ error });
+  }
 });
 
 app.listen(port, () => {
